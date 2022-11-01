c7a0a328991988493367b49152bcee4ce3f99d51
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-11-01 03:39:41 +0000

    commenting my thought process through code

---
+++ .ccls-cache/@home@runner@Problem-5/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+const int ROWS = 2;
+const int COLS = 3;
+void arr(int[][COLS]); // function prototype
+
+int main() {
+
+  int nums[ROWS][COLS] = {{33, 16, 29}, {54, 67, 99}}; // int matrix
+
+  arr(nums); // calling function, accepting nums matrix?
+  
+  return 0;
+}
+
+void arr(int (*val)[3]) {
+
+  cout << endl << *(*val);
+  cout << endl << *(*val + 1);
+  cout << endl << *(*(val + 1) + 2);
+  cout << endl << *(*val) + 1;
+
+  return;
+}
