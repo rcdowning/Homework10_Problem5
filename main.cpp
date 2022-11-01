3366f0639b8d17630c11d3db3015063ec3b1bbb6
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-11-01 04:11:16 +0000

    Function outputs found

--- main.cpp
+++ main.cpp
@@ -18,10 +18,10 @@ int main() {
 
 void arr(int (*val)[3]) {  // function using matrix pointer variable named *val with 3 columns
 
-  cout << endl << *(*val);
-  cout << endl << *(*val + 1);
-  cout << endl << *(*(val + 1) + 2);
-  cout << endl << *(*val) + 1;
+  cout << endl << *(*val);           // points to val[0] = 33
+  cout << endl << *(*val + 1);       // points to val[0] + 1 = val[2] = 16
+  cout << endl << *(*(val + 1) + 2); // val + 1 = sec row, 2 means 0, 1st, 2nd column = 99
+  cout << endl << *(*val) + 1;       // points to val[0] value, then adds 1 = 34
 
   return;
 }
